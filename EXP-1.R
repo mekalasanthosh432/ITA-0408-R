2f7a5dcb45ff13791ab899bf3e067e58749549ae
mekalasanthosh432 <msanthosh9391@gmail.com>
2026-08-27 13:59:40 +0530

    Create EXP-1.R

---
+++ EXP-1.R
@@ -0,0 +1,18 @@
+# Numeric vector
+num_vec <- c(10, 20, 30, 40, 50)
+
+# Character vector
+char_vec <- c("Apple", "Banana", "Mango", "Orange")
+
+# Logical vector
+logical_vec <- c(TRUE, FALSE, TRUE, TRUE, FALSE)
+
+# Display type and content
+print(num_vec)
+print(typeof(num_vec))
+
+print(char_vec)
+print(typeof(char_vec))
+
+print(logical_vec)
+print(typeof(logical_vec))
